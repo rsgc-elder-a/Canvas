ea29d06f75fd537e870e55dd285f7001c65edd25
Andrew <aelder@rsgc.on.ca>
2017-04-21 14:14:19 -0400

    Does not work right now by getting some generation stuff working.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -6,11 +6,11 @@
  
  - callout(Experiment):
  
-    Uncomment line 27 so that the axes of the plane are automatically drawn.
+ Uncomment line 27 so that the axes of the plane are automatically drawn.
  
-    Where is the origin?
+ Where is the origin?
  
-    In what direction do values on the *x* and *y* axes increase?
+ In what direction do values on the *x* and *y* axes increase?
  
  */
 // These are some required statements to make this playground work.
@@ -19,6 +19,9 @@ import PlaygroundSupport
 
 // Create a new canvas
 let canvas = Canvas(width: 500, height: 500)
+
+//for reducing the size
+//currentLegnth = Int(Double(initialLegnth) / pow(Double(reduction), Double(n)))
 
 // View the current state of the canvas
 canvas
@@ -30,41 +33,73 @@ canvas.drawAxes()
 
 let f = 100
 let angle = Degrees(60)
-let word = "F-F++F-F++F-F++F-F++F-F++F-F"
+//let word = "F-F++F-F++F-F++F-F++F-F++F-F"
+//let word = "--FF+F+FF+F+FF"
+//let word = "F-F++F-F-F-F+F-F++F-F++F-F-F-F++F"
+let base = "F++F++F"
+let rule = "F-F++F-"
 var xVal = f //starting values
 var yVal = f
 
 var first = true;
-for i in word.characters {
-    //must use xVal and yVal to be actual values
-    if(i == "F"){
-        if first == true {
-        canvas.translate(byX: xVal, byY: yVal)
-        }
-        canvas.drawLine(fromX: 0, fromY: 0, toX: f, toY: 0)
-        canvas.translate(byX: f, byY: 0)
 
+func drawDo(input: String) {
+    for i in input.characters {
+        //must use xVal and yVal to be actual values
+        if(i == "F"){
+            if first == true {
+                canvas.translate(byX: xVal, byY: yVal)
+            }
+            canvas.drawLine(fromX: 0, fromY: 0, toX: f, toY: 0)
+            canvas.translate(byX: f, byY: 0)
+            
+        }
+        
+        if(i == "-"){
+            //canvas.translate(byX: xVal, byY: yVal)
+            if first == true {
+                canvas.translate(byX: xVal, byY: yVal)
+            }
+            canvas.rotate(by: -angle)
+            
+            //canvas.translate(byX: -xVal, byY: -yVal)
+            
+        }
+        
+        if(i == "+"){
+            //canvas.translate(byX: xVal, byY: yVal)
+            if first == true {
+                canvas.translate(byX: xVal, byY: yVal)
+            }
+            canvas.rotate(by: angle)
+            
+            //canvas.translate(byX: -xVal, byY: -yVal)
+            
+            
+        }
+        first = false
     }
-    
-    if(i == "-"){
-        //canvas.translate(byX: xVal, byY: yVal)
-        canvas.rotate(by: -angle)
-        
-        //canvas.translate(byX: -xVal, byY: -yVal)
-        
-    }
-    
-    if(i == "+"){
-        //canvas.translate(byX: xVal, byY: yVal)
-        canvas.rotate(by: angle)
-        
-        //canvas.translate(byX: -xVal, byY: -yVal)
-        
-        
-    }
- first = false
 }
 
+func createLay(base: String, rule: String, iterations: Int){
+    var newString = base
+    for i in 0...iterations{
+       
+        for charicter in newString.characters {
+            if charicter == "F" {
+                
+                newString.append(rule)
+                
+            } else {
+                newString.append(charicter)
+            }
+        }
+        
+    }
+    print(newString);
+}
+
+createLay(base: base, rule: rule, iterations: 0);
 /*:
  
  ## To see output
@@ -78,6 +113,6 @@ for i in word.characters {
  ![timeline-option](timeline-option.png "Show the timeline")
  
  Finally, do not remove the following line of code:
-
+ 
  */
 PlaygroundPage.current.liveView = canvas.imageView
